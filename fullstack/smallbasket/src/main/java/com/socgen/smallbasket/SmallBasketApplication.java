bdf258caac3980aa2c404399f335d74db36c5ef2
Aditi Parahsar <aditiparashar95@gmail.com>
2020-08-23 00:01:01 +0530

    Fullstack initial commit; packaging react app and java server into a single jar

---
+++ fullstack/smallbasket/src/main/java/com/socgen/smallbasket/SmallBasketApplication.java
@@ -0,0 +1,13 @@
+package com.socgen.smallbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBasketApplication.class, args);
+	}
+
+}
